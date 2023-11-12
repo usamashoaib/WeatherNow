572737a4df4b920596bc3256dd84f960212463ed
Usama Shoaib <usama.shoaib786610@gmail.com>
2023-11-13 00:13:19 +0500

    Adding basics spring security v6.1.5.

---
+++ src/main/java/com/example/WeatherNow/config/SpringSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.WeatherNow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
